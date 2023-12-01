3d42bd6d7616eb442eb5c5a68ef19f2607152301
Will Koury <wkoury@hey.com>
2023-12-01 10:07:55 -0500

    Day 1 part 1
    	day1/input.txt

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	contentString := string(content)
+	lines := strings.Split(contentString, "\n")
+
+	sum := 0
+
+	for ii, line := range lines {
+		if ii < len(lines)-1 {
+
+			// Sentinel values
+			firstNumber := -1
+			lastNumber := -1
+
+			re := regexp.MustCompile("[0-9]")
+
+			// Find the first number
+			for ii := range line {
+				if firstNumber == -1 && re.Match([]byte{line[ii]}) {
+					firstNumber, _ = strconv.Atoi(string(line[ii]))
+				}
+			}
+
+			// Find the last number
+			for ii := len(line) - 1; ii >= 0; ii-- {
+				if lastNumber == -1 && re.Match([]byte{line[ii]}) {
+					lastNumber, _ = strconv.Atoi(string(line[ii]))
+				}
+			}
+
+			sum += firstNumber*10 + lastNumber
+		}
+	}
+
+	fmt.Println(sum)
 }
